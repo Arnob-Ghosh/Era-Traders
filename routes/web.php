0ce0b97db5ebf5f8155d5495273753b8a0777d40
Arnob Ghosh <arnobghosh.go@gmail.com>
2024-12-04 00:26:33 +0600

    clear route

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/login');
 });
+Route::get('/clear', function () {
+    // Run Artisan commands
+    Artisan::call('optimize:clear');
+    return redirect()->back();
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
